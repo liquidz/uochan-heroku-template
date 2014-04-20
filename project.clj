a1204f81c0a7268b9e08df3795f3a4e4b9d1da62
liquidz <liquidz.uo@gmail.com>
2014-04-21 07:42:11 +0900

    fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject uochan-heroku/lein-template "0.1.0-SNAPSHOT"
+(defproject uochan-heroku/lein-template "0.0.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
